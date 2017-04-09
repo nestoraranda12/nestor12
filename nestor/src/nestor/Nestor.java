9dd34831bf9df256388cf2caa256a159838bad77
pc <pc@172.16.12.122>
2017-04-09 11:00:30 -0600

    Inicio del proyecto

---
+++ nestor/src/nestor/Nestor.java
@@ -0,0 +1,17 @@
+
+package nestor;
+
+/**
+ *
+ * @author pc
+ */
+public class Nestor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
